78104251eef52a9a3198a4a7a09777df29ae84ba
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-04-18 10:09:05 +0530

    Added say command and  other commands related to it

--- Cogs/utilities.py
+++ Cogs/utilities.py
@@ -4,6 +4,9 @@
 
 import discord
 from discord.ext import commands
+from discord.utils import escape_mentions
+from lib.db import db
+from aiohttp import ClientSession
 
 
 class Utilities(commands.Cog):
@@ -24,11 +27,60 @@ class Utilities(commands.Cog):
         embed.set_image(url=member.avatar_url)
         await ctx.channel.send(embed=embed)
 
+    @commands.command(name="set_webhook")
+    async def set_bot_webhook(self, ctx):
+        guild_id = db.cursor.execute("SELECT 1 FROM webhook WHERE GuildID = ? ",
+                                     (ctx.message.guild.id,))
+        guild_id_exists = guild_id.fetchone() is not None
+        if guild_id_exists is False:
+            created_webhook = await ctx.channel.create_webhook(name="SayCmd Webhook")
+            url = created_webhook.url
+            db.execute("INSERT OR IGNORE INTO webhook(GuildID, Url) VALUES(?, ?)", int(ctx.message.guild.id), str(url))
+            db.commit()
+            await ctx.channel.send('⚒  Webhook has been created for this channel')
+        elif guild_id_exists is True:
+            await ctx.channel.send(
+                "You have already set the webhook for this channel. For resetting, delete the exisiting webhook by using `delete_webhook` and then type this command.")
+
+    @commands.command(name="delete_webhook")
+    async def delete_bot_webhook(self, ctx):
+        guild_id = db.cursor.execute("SELECT 1 FROM webhook WHERE GuildID = ? ",
+                                     (ctx.message.guild.id,))
+        guild_id_exists = guild_id.fetchone() is not None
+        if guild_id_exists is False:
+            await ctx.channel.send(
+                'You cannot delete a webhook unless u have created it. Run `set_webhook` to create a webhook first')
+        elif guild_id_exists is True:
+            db.execute("DELETE FROM webhook WHERE GuildID = ?", (ctx.message.guild.id))
+            db.commit()
+            await ctx.channel.send("⚒  The webhook for this channel has been deleted from the database.")
+
+    @commands.command(name="say")
+    async def say_webhook_command(self, message, *, query: str = 'hello!'):
+
+        async with ClientSession() as session:
+            guild_id = db.cursor.execute("SELECT 1 FROM webhook WHERE GuildID = ? ",
+                                         (message.guild.id,))
+            guild_id_exists = guild_id.fetchone() is not None
+            if not guild_id_exists:
+                await message.channel.send(
+                    "Oops! Tell a mod in the server to run the `set_webhook` command before using this command :)")
+            else:
+                query = escape_mentions(query)
+                webhook_url = db.field("SELECT Url FROM webhook WHERE GuildID = ?", (message.guild.id))
+
+                # print(message.guild.id) Used it for debugging.
+                # print(webhook_url) Used it for debugging.
+            webhook = discord.Webhook.from_url(webhook_url, adapter=discord.AsyncWebhookAdapter(session))
+            await message.channel.purge(limit=1)
+            await webhook.send(content=query, username=message.author.name, avatar_url=message.author.avatar_url)
+
     @commands.command(name="link")
     async def send_bot_invite_link(self, ctx):
         embed = discord.Embed(title="Invite Me 🥰", timestamp=ctx.message.created_at, color=discord.Colour.dark_gold(),
                               description="Hey there! click on [this link](https://discord.com/oauth2/authorize?client_id=830047831972118588&permissions=109638&scope=bot) to invite me to your server!")
-        embed.set_thumbnail(url="https://static.wixstatic.com/media/6e38f1_72944a54fe214e029653f12798bb8136~mv2.png/v1/fill/w_560,h_210,al_c,q_85,usm_0.66_1.00_0.01/6e38f1_72944a54fe214e029653f12798bb8136~mv2.webp")
+        embed.set_thumbnail(
+            url="https://static.wixstatic.com/media/6e38f1_72944a54fe214e029653f12798bb8136~mv2.png/v1/fill/w_560,h_210,al_c,q_85,usm_0.66_1.00_0.01/6e38f1_72944a54fe214e029653f12798bb8136~mv2.webp")
 
         await ctx.channel.send(embed=embed)
 
