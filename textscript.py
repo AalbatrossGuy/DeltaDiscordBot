c48475ae4e985437f226994661e3af9798ea4666
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2021-04-25 08:48:10 +0530

    Writing the api for the command (beta-state)

--- textscript.py
+++ textscript.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw, ImageFilter
+import requests
 
 
 #
@@ -20,60 +21,85 @@ def mask_circle_transparent(pil_img, blur_radius, offset=0):
     return result
 
 
-# img1 = Image.open(r"Pictures/WelcomeMsg.png")
+# # img1 = Image.open(r"Pictures/WelcomeMsg.png")
+# #
+# # draw = ImageDraw.Draw(img1)
+# # print(img1.size)
+# # font = ImageFont.truetype(r"Fonts/ZenDots-Regular.ttf", 30)
+# #
+# # draw.text((400, 100), f"Welcome to Test Server", (0, 0, 0), font=font)
+# # draw.text((350, 500), f"You are member no. #20000", (0, 0, 0), font=font)
+# # # Opening the secondary image (overlay image)
+# # img2 = Image.open(r"Pictures/test.png")
+# #
+# # # Pasting img2 image on top of img1
+# # # starting at coordinates (0, 0)
+# # img1.paste(img2, (469, 180), mask=img2)
+# #
+# # # Displaying the image
+# # img1.show()
 #
-# draw = ImageDraw.Draw(img1)
-# print(img1.size)
-# font = ImageFont.truetype(r"Fonts/ZenDots-Regular.ttf", 30)
 #
-# draw.text((400, 100), f"Welcome to Test Server", (0, 0, 0), font=font)
-# draw.text((350, 500), f"You are member no. #20000", (0, 0, 0), font=font)
-# # Opening the secondary image (overlay image)
-# img2 = Image.open(r"Pictures/test.png")
+# img1 = Image.open(r"Pictures/WelcomeImg.jpg")
 #
-# # Pasting img2 image on top of img1
-# # starting at coordinates (0, 0)
-# img1.paste(img2, (469, 180), mask=img2)
+#
+# # draw = ImageDraw.Draw(img1)
+# # print(img1.size)
+# # font = ImageFont.truetype(r"Fonts/ZenDots-Regular.ttf", 30)
+# #
+# # draw.text((400, 100), f"Welcome to Test Server", (0, 0, 0), font=font)
+# # draw.text((350, 500), f"You are member no. #20000", (0, 0, 0), font=font)
+# # # Opening the secondary image (overlay image)
+# # img2 = Image.open(r"Pictures/test.png")
+# #
+# # # Pasting img2 image on top of img1
+# # # starting at coordinates (0, 0)
+# # img1.paste(img2, (469, 180), mask=img2)
 #
 # # Displaying the image
-# img1.show()
-
-
-img1 = Image.open(r"Pictures/WelcomeImg.jpg")
-
-
-# draw = ImageDraw.Draw(img1)
-# print(img1.size)
-# font = ImageFont.truetype(r"Fonts/ZenDots-Regular.ttf", 30)
+# # img1.show()
 #
-# draw.text((400, 100), f"Welcome to Test Server", (0, 0, 0), font=font)
-# draw.text((350, 500), f"You are member no. #20000", (0, 0, 0), font=font)
-# # Opening the secondary image (overlay image)
-# img2 = Image.open(r"Pictures/test.png")
 #
-# # Pasting img2 image on top of img1
-# # starting at coordinates (0, 0)
-# img1.paste(img2, (469, 180), mask=img2)
+# def _add_corners(im, rad=100):
+#     circle = Image.new('L', (rad * 2, rad * 2), 0)
+#     draw = ImageDraw.Draw(circle)
+#     draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
+#     alpha = Image.new('L', im.size, "white")
+#     w, h = im.size
+#     alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
+#     alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
+#     alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
+#     alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
+#     im.putalpha(alpha)
+#     return im
+#
+#
+# im = _add_corners(img1)
 
-# Displaying the image
-# img1.show()
+l = []
 
-
-def _add_corners(im, rad=100):
-    circle = Image.new('L', (rad * 2, rad * 2), 0)
-    draw = ImageDraw.Draw(circle)
-    draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
-    alpha = Image.new('L', im.size, "white")
-    w, h = im.size
-    alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
-    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
-    alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
-    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
-    im.putalpha(alpha)
-    return im
-
-
-im = _add_corners(img1)
 # print(im.show())
 
+"""This is a test for a movie based api command. The command is not complete yet."""
 
+
+def api(query: str):
+    url = f"https://www.omdbapi.com/?t={query}&apikey=706a1bfd"
+    print(url)
+    response = requests.request("GET", url=url)
+    data = response.json()
+    print(data)
+    title = data['Title']
+    year = data['Year']
+    rated = data['Rated']
+    released = data['Released']
+    length = data['Runtime']
+    genre = data['Genre']
+    director = data['Director']
+    writer = data['Writer']
+    actors = data['Actors']
+    language = data['Language']
+    country = data['Country']
+
+
+api('Bloodshot')
