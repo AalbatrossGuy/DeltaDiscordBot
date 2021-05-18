014fd3c76737a3dc5877a07b17c81afae377a0dd
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-05-18 15:20:08 +0530

    Added password generator command

--- Cogs/utilities_and_fun.py
+++ Cogs/utilities_and_fun.py
@@ -8,6 +8,8 @@ from discord.utils import escape_mentions
 from lib.db import db
 from aiohttp import ClientSession
 import requests
+import random
+import array
 
 
 def convert_bytes(bytes_number):
@@ -237,6 +239,57 @@ class Utilities(commands.Cog):
         embed.set_footer(text="Delta Δ is the fourth letter of the Greek Alphabet", icon_url=ctx.author.avatar_url)
         embed.set_thumbnail(url=member_avatar_url)
         await ctx.channel.send(embed=embed)
+
+    @commands.command(name="paswdgen")
+    async def password_generator(self, ctx, len:int):
+        if len > 50:
+            await ctx.channel.send("Length Cannot Be More Than `50` Characters.")
+        else:
+            MAX_LEN = len
+            
+        
+            DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
+            LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                                'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                                'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                                'z']
+            
+            UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
+                                'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                                'Z']
+            
+            SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+                    '*', '(', ')', '<']
+            
+
+            COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+            
+
+            rand_digit = random.choice(DIGITS)
+            rand_upper = random.choice(UPCASE_CHARACTERS)
+            rand_lower = random.choice(LOCASE_CHARACTERS)
+            rand_symbol = random.choice(SYMBOLS)
+            
+
+            temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
+            
+            
+
+            for x in range(MAX_LEN - 4):
+                temp_pass = temp_pass + random.choice(COMBINED_LIST)
+            
+
+                temp_pass_list = array.array('u', temp_pass)
+                random.shuffle(temp_pass_list)
+            
+
+            password = ""
+            for x in temp_pass_list:
+                    password = password + x
+                    
+            
+            await ctx.channel.send(f"🔑 Here's Your Password\n```yaml\n{password}```")
 
 
 def setup(client):
