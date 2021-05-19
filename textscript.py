6ddef8479e3132ab3e9ce33bb5331a833c78f9ba
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-05-19 13:28:57 +0530

    Example Test for Weather API

--- textscript.py
+++ textscript.py
@@ -1,77 +1,77 @@
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 import requests
-
-
+import pytemperature
+import datetime
 # print(image.size)
 # print(image.format)
 # print(image.show())
 
 
-def mask_circle_transparent(pil_img, blur_radius, offset=0):
-    offset = blur_radius * 2 + offset
-    mask = Image.new("L", pil_img.size, 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((offset, offset, pil_img.size[0] - offset, pil_img.size[1] - offset), fill=255)
-    mask = mask.filter(ImageFilter.GaussianBlur(blur_radius))
+# def mask_circle_transparent(pil_img, blur_radius, offset=0):
+#     offset = blur_radius * 2 + offset
+#     mask = Image.new("L", pil_img.size, 0)
+#     draw = ImageDraw.Draw(mask)
+#     draw.ellipse((offset, offset, pil_img.size[0] - offset, pil_img.size[1] - offset), fill=255)
+#     mask = mask.filter(ImageFilter.GaussianBlur(blur_radius))
 
-    result = pil_img.copy()
-    result.putalpha(mask)
+#     result = pil_img.copy()
+#     result.putalpha(mask)
 
-    return result
+#     return result
 
 
-img1 = Image.open(r"Pictures/WelcomeMsg.png")
+# img1 = Image.open(r"Pictures/WelcomeMsg.png")
 
-draw = ImageDraw.Draw(img1)
-print(img1.size)
-font = ImageFont.truetype(r"Fonts/ZenDots-Regular.ttf", 30)
+# draw = ImageDraw.Draw(img1)
+# print(img1.size)
+# font = ImageFont.truetype(r"Fonts/ZenDots-Regular.ttf", 30)
 
-draw.text((400, 100), f"Welcome to Test Server", (0, 0, 0), font=font)
-draw.text((350, 500), f"You are member no. #20000", (0, 0, 0), font=font)
-# # Opening the secondary image (overlay image)
-img2 = Image.open(r"Pictures/test.png")
-#
-# # Pasting img2 image on top of img1
-# # starting at coordinates (0, 0)
-img1.paste(img2, (469, 180), mask=img2)
-#
+# draw.text((400, 100), f"Welcome to Test Server", (0, 0, 0), font=font)
+# draw.text((350, 500), f"You are member no. #20000", (0, 0, 0), font=font)
+# # # Opening the secondary image (overlay image)
+# img2 = Image.open(r"Pictures/test.png")
+# #
+# # # Pasting img2 image on top of img1
+# # # starting at coordinates (0, 0)
+# img1.paste(img2, (469, 180), mask=img2)
+# #
+# # # Displaying the image
+# img1.show()
+
+# img1 = Image.open(r"Pictures/WelcomeImg.jpg")
+
+# draw = ImageDraw.Draw(img1)
+# print(img1.size)
+# font = ImageFont.truetype(r"Fonts/ZenDots-Regular.ttf", 30)
+
+# draw.text((400, 100), f"Welcome to Test Server", (0, 0, 0), font=font)
+# draw.text((350, 500), f"You are member no. #20000", (0, 0, 0), font=font)
+# # # Opening the secondary image (overlay image)
+# img2 = Image.open(r"Pictures/test.png")
+# #
+# # # Pasting img2 image on top of img1
+# # # starting at coordinates (0, 0)
+# img1.paste(img2, (469, 180), mask=img2)
+
 # # Displaying the image
-img1.show()
-
-img1 = Image.open(r"Pictures/WelcomeImg.jpg")
-
-draw = ImageDraw.Draw(img1)
-print(img1.size)
-font = ImageFont.truetype(r"Fonts/ZenDots-Regular.ttf", 30)
-
-draw.text((400, 100), f"Welcome to Test Server", (0, 0, 0), font=font)
-draw.text((350, 500), f"You are member no. #20000", (0, 0, 0), font=font)
-# # Opening the secondary image (overlay image)
-img2 = Image.open(r"Pictures/test.png")
-#
-# # Pasting img2 image on top of img1
-# # starting at coordinates (0, 0)
-img1.paste(img2, (469, 180), mask=img2)
-
-# Displaying the image
-img1.show()
+# img1.show()
 
 
-def _add_corners(im, rad=100):
-    circle = Image.new('L', (rad * 2, rad * 2), 0)
-    draw = ImageDraw.Draw(circle)
-    draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
-    alpha = Image.new('L', im.size, "white")
-    w, h = im.size
-    alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
-    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
-    alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
-    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
-    im.putalpha(alpha)
-    return im
+# def _add_corners(im, rad=100):
+#     circle = Image.new('L', (rad * 2, rad * 2), 0)
+#     draw = ImageDraw.Draw(circle)
+#     draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
+#     alpha = Image.new('L', im.size, "white")
+#     w, h = im.size
+#     alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
+#     alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
+#     alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
+#     alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
+#     im.putalpha(alpha)
+#     return im
 
 
-im = _add_corners(img1)
+# im = _add_corners(img1)
 
 
 # print(im.show())
@@ -105,4 +105,37 @@ def test_for_api(query: str):
 
 
 # Test Query
-test_for_api("Moana")
+# test_for_api("Moana")
+
+def test_for_weather_api(query:str):
+    url = f"http://api.openweathermap.org/data/2.5/weather?appid=77f5585dd715ec1e39ba87b818b44498&q={query}"
+
+    response = requests.request("GET", url=url)
+    # For debugging purpose
+    # print(response)
+    data = response.json()
+    print(data)
+    longitude = data['coord']['lon']
+    latitude = data['coord']['lat']
+    # print(longitude, latitude)
+    cloud_type = data['weather'][0]['description']
+    print(cloud_type)
+    temp = data['main']['temp']
+    feels_like = data['main']['feels_like']
+    temp = pytemperature.k2c(temp).__format__('0.2f')
+    feels_like = pytemperature.k2c(feels_like).__format__('0.2f')
+    print(temp, feels_like)
+    min_temp = data['main']['temp_min']
+    min_temp = pytemperature.k2c(min_temp).__format__('0.2f')
+    max_temp = data['main']['temp_max']
+    max_temp = pytemperature.k2c(max_temp).__format__('0.2f')
+    pressure = data['main']['pressure'] # unit hPa
+    humidity = data['main']['humidity'] # unit percentage
+    wind_speed = data['wind']['speed']
+    print(min_temp, max_temp, pressure, humidity, wind_speed)
+    country = data['sys']['country']
+    city_name = data['name']
+
+
+
+test_for_weather_api("delhi")
