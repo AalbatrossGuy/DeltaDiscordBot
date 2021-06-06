9c9d6e2dd60385ae1952e270675b114c45024df1
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-06-07 02:50:09 +0530

    Added new test code

--- textscript.py
+++ textscript.py
@@ -78,63 +78,114 @@ import datetime
 """This is a test for a movie based api command. The command is not complete yet."""
 
 
-def test_for_api(query: str):
-    url = f"https://www.omdbapi.com/?t={query}&apikey=706a1bfd"
-    print(url)
-    response = requests.request("GET", url=url)
-    data = response.json()
-    print(data)
-    title = data['Title']
-    year = data['Year']
-    rated = data['Rated']
-    released = data['Released']
-    length = data['Runtime']
-    genre = data['Genre']
-    director = data['Director']
-    writer = data['Writer']
-    actors = data['Actors']
-    language = data['Language']
-    country = data['Country']
-    awards = data['Awards']
-    Poster_Img = data['Poster']
-    rating = data['imdbRating']
-    votes = data['imdbVotes']
-    type = data['movie']
-    boxoffice = data['BoxOffice']
+# def test_for_api(query: str):
+#     url = f"https://www.omdbapi.com/?t={query}&apikey=706a1bfd"
+#     print(url)
+#     response = requests.request("GET", url=url)
+#     data = response.json()
+#     print(data)
+#     title = data['Title']
+#     year = data['Year']
+#     rated = data['Rated']
+#     released = data['Released']
+#     length = data['Runtime']
+#     genre = data['Genre']
+#     director = data['Director']
+#     writer = data['Writer']
+#     actors = data['Actors']
+#     language = data['Language']
+#     country = data['Country']
+#     awards = data['Awards']
+#     Poster_Img = data['Poster']
+#     rating = data['imdbRating']
+#     votes = data['imdbVotes']
+#     type = data['movie']
+#     boxoffice = data['BoxOffice']
 
 
 # Test Query
 # test_for_api("Moana")
 
-def test_for_weather_api(query:str):
-    url = f"http://api.openweathermap.org/data/2.5/weather?appid=77f5585dd715ec1e39ba87b818b44498&q={query}"
+# def test_for_weather_api(query:str):
+#     url = f"http://api.openweathermap.org/data/2.5/weather?appid=77f5585dd715ec1e39ba87b818b44498&q={query}"
 
-    response = requests.request("GET", url=url)
-    # For debugging purpose
-    # print(response)
-    data = response.json()
-    # print(data)
-    longitude = data['coord']['lon']
-    latitude = data['coord']['lat']
-    # print(longitude, latitude)
-    cloud_type = data['weather'][0]['description']
-    # print(cloud_type)
-    temp = data['main']['temp']
-    feels_like = data['main']['feels_like']
-    temp = (temp - 273.15).__format__('0.2f')
-    feels_like = (feels_like - 273.15).__format__('0.2f')
-    # print(temp, feels_like)
-    min_temp = data['main']['temp_min']
-    min_temp = (min_temp - 273.15).__format__('0.2f')
-    max_temp = data['main']['temp_max']
-    max_temp = (max_temp - 273.15).__format__('0.2f')
-    pressure = data['main']['pressure'] # unit hPa
-    humidity = data['main']['humidity'] # unit percentage
-    wind_speed = data['wind']['speed']
-    # print(min_temp, max_temp, pressure, humidity, wind_speed)
-    country = data['sys']['country']
-    city_name = data['name']
+#     response = requests.request("GET", url=url)
+#     # For debugging purpose
+#     # print(response)
+#     data = response.json()
+#     # print(data)
+#     longitude = data['coord']['lon']
+#     latitude = data['coord']['lat']
+#     # print(longitude, latitude)
+#     cloud_type = data['weather'][0]['description']
+#     # print(cloud_type)
+#     temp = data['main']['temp']
+#     feels_like = data['main']['feels_like']
+#     temp = (temp - 273.15).__format__('0.2f')
+#     feels_like = (feels_like - 273.15).__format__('0.2f')
+#     # print(temp, feels_like)
+#     min_temp = data['main']['temp_min']
+#     min_temp = (min_temp - 273.15).__format__('0.2f')
+#     max_temp = data['main']['temp_max']
+#     max_temp = (max_temp - 273.15).__format__('0.2f')
+#     pressure = data['main']['pressure'] # unit hPa
+#     humidity = data['main']['humidity'] # unit percentage
+#     wind_speed = data['wind']['speed']
+#     # print(min_temp, max_temp, pressure, humidity, wind_speed)
+#     country = data['sys']['country']
+#     city_name = data['name']
 
 
 
-test_for_weather_api("delhi")
+# test_for_weather_api("delhi")
+
+""" Horizontal Bar Chart related tests"""
+
+# import matplotlib.pyplot as plt 
+# import numpy as np 
+# import qrcode
+# from PIL import Image
+
+
+# np.random.seed(19680801)
+# def checkesc(args):
+#     args = list(args)
+#     for i in range(len(args)):
+#         if args[i] == '|':
+#             return False
+#         else:
+#             return True
+
+
+# def makebar(args):
+#     args = str(args).replace(' ', '')
+#     args = list(args)
+#     tupbefore = []
+#     tupafter = []
+#     index = args.index('|')
+#     tupbefore = args[0:index]
+#     tupafter = args[index+1:]
+#     # print(tupbefore)
+#     # print(tupafter)
+        
+#     # plt.rcdefaults()
+#     # fig, axes = plt.subplots()
+#     # numbers = tupbefore
+#     # labels = tupafter
+#     # axes.barh(labels, numbers, align='center')
+#     # plt.show()
+
+
+# print(makebar('1 2 3 4 | 9 6 7 8'))
+
+# qr = qrcode.QRCode(
+#     version=1,
+#     error_correction=qrcode.constants.ERROR_CORRECT_H,
+#     box_size=10,
+#     border=4,
+# )
+# qr.add_data('THis is an example test line')
+# qr.make(fit=True)
+# img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
+
+# img.show()
