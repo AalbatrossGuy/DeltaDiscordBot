9f2789b5af4239116e7ecfe34498114186f24bf8
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-07-30 18:11:59 +0530

    Added twt command

--- Cogs/fun.py
+++ Cogs/fun.py
@@ -41,6 +41,30 @@ class Fun(commands.Cog):
                     await ctx.send('Oops! An error occured. Please Try Again Later.')
                 await session.close()
 
+    @commands.command(name="twt")
+    async def tweet_fake(self, ctx, username: discord.Member, *, comment):
+        avatar = username.avatar_url_as(format='png')
+        comment = comment.replace(" ", "+")
+        if username.nick == None : nick_name = str(username).split("#")[0]
+        else: nick_name = username.nick
+        #url = "https://some-random-api.ml/canvas/tweet?avatar=https://cdn.discordapp.com/avatars/560789882738573324/bc220b0eeeabbe234026e4881f3c3b9c.png&username=Telk&displayname=TAl&comment=Hello"
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'https://some-random-api.ml/canvas/tweet?avatar={avatar}&username={str(username).split("#")[0]}&displayname={nick_name}&comment={comment}'
+            ) as af:
+                if 300 > af.status >= 200:
+                    fp = io.BytesIO(await af.read())
+                    file = discord.File(fp, "tweet.png")
+                    em = discord.Embed(
+                        title="Nice Tweet ;)",
+                        color=discord.Color.dark_red(),
+                    )
+                    em.set_image(url="attachment://tweet.png")
+                    await ctx.send(embed=em, file=file)
+                    
+                else:
+                    await ctx.send('Oops! An error occured. Please Try Again Later.')
+                await session.close()
 
 def setup(client):
     client.add_cog(Fun(client))
