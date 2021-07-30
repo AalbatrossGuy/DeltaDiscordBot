384b1b2c128cc6cbc3b74f3edca66e9159f00a2d
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-07-30 17:10:11 +0530

    Added ytcomm command

--- Cogs/fun.py
+++ Cogs/fun.py
@@ -2,13 +2,14 @@
 # !python3
 # /usr/bin/env python3
 
-import discord, random
+import discord, random, requests
 from discord.ext import commands
-
+import aiohttp, io
 
 class Fun(commands.Cog):
     def __init__(self, client):
         self.client = client
+        
 
     @commands.command(name="predict")
     async def predict(self, ctx, *, question:str):
@@ -18,8 +19,28 @@ class Fun(commands.Cog):
 
         await ctx.channel.send(f"{random.random(responses)}")
 
+    @commands.command(name='ytcomm')
+    async def youtube_comment_fake(self, ctx, username: discord.Member, *, comment):
+        avatar = username.avatar_url_as(format='png')
+        # str(username).split('#')[0] strip the discriminator
+        comment = comment.replace(" ", "+")
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'https://some-random-api.ml/canvas/youtube-comment?username={str(username).split("#")[0]}&comment={comment}&avatar={avatar}'
+            ) as af:
+                if 300 > af.status >= 200:
+                    fp = io.BytesIO(await af.read())
+                    file = discord.File(fp, "ytcomment.png")
+                    em = discord.Embed(
+                        title="Nice Comment ;)",
+                        color=discord.Color.dark_red(),
+                    )
+                    em.set_image(url="attachment://ytcomment.png")
+                    await ctx.send(embed=em, file=file)
+                    
+                else:
+                    await ctx.send('Oops! An error occured. Please Try Again Later.')
+                await session.close()
 
-    
 
 def setup(client):
     client.add_cog(Fun(client))
