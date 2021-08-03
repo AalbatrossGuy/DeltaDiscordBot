d75c4c33ba497450acb0d1375f4c5ca6dc0f5e88
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2021-08-03 10:43:50 +0530

    Added line spaces for code readability

--- Cogs/fun.py
+++ Cogs/fun.py
@@ -9,11 +9,9 @@ from discord_components import DiscordComponents, Select, SelectOption
 from datetime import timedelta
 
 
-
 class Fun(commands.Cog):
     def __init__(self, client):
         self.client = client
-
 
     @commands.command(name='ytcomm')
     async def youtube_comment_fake(self, ctx, username: discord.Member, *, comment):
@@ -21,8 +19,9 @@ class Fun(commands.Cog):
         # str(username).split('#')[0] strip the discriminator
         comment = comment.replace(" ", "+")
         async with aiohttp.ClientSession() as session:
-            async with session.get(f'https://some-random-api.ml/canvas/youtube-comment?username={str(username).split("#")[0]}&comment={comment}&avatar={avatar}'
-            ) as af:
+            async with session.get(
+                    f'https://some-random-api.ml/canvas/youtube-comment?username={str(username).split("#")[0]}&comment={comment}&avatar={avatar}'
+                    ) as af:
                 if 300 > af.status >= 200:
                     fp = io.BytesIO(await af.read())
                     file = discord.File(fp, "ytcomment.png")
@@ -32,7 +31,7 @@ class Fun(commands.Cog):
                     )
                     em.set_image(url="attachment://ytcomment.png")
                     await ctx.reply(embed=em, file=file, mention_author=False)
-                    
+
                 else:
                     await ctx.reply('Oops! An error occured. Please Try Again Later.')
                 await session.close()
@@ -41,13 +40,16 @@ class Fun(commands.Cog):
     async def tweet_fake(self, ctx, username: discord.Member, *, comment):
         avatar = username.avatar_url_as(format='png')
         comment = comment.replace(" ", "+")
-        if username.nick == None : nick_name = str(username).split("#")[0]
-        else: nick_name = username.nick
-        #url = "https://some-random-api.ml/canvas/tweet?avatar=https://cdn.discordapp.com/avatars/560789882738573324/bc220b0eeeabbe234026e4881f3c3b9c.png&username=Telk&displayname=TAl&comment=Hello"
-        
+        if username.nick == None:
+            nick_name = str(username).split("#")[0]
+        else:
+            nick_name = username.nick
+        # url = "https://some-random-api.ml/canvas/tweet?avatar=https://cdn.discordapp.com/avatars/560789882738573324/bc220b0eeeabbe234026e4881f3c3b9c.png&username=Telk&displayname=TAl&comment=Hello"
+
         async with aiohttp.ClientSession() as session:
-            async with session.get(f'https://some-random-api.ml/canvas/tweet?avatar={avatar}&username={str(username).split("#")[0]}&displayname={nick_name}&comment={comment}'
-            ) as af:
+            async with session.get(
+                    f'https://some-random-api.ml/canvas/tweet?avatar={avatar}&username={str(username).split("#")[0]}&displayname={nick_name}&comment={comment}'
+                    ) as af:
                 if 300 > af.status >= 200:
                     fp = io.BytesIO(await af.read())
                     file = discord.File(fp, "tweet.png")
@@ -57,33 +59,36 @@ class Fun(commands.Cog):
                     )
                     em.set_image(url="attachment://tweet.png")
                     await ctx.reply(embed=em, file=file, mention_author=False)
-                    
+
                 else:
                     await ctx.reply('Oops! An error occured. Please Try Again Later.')
                 await session.close()
-    
+
     @commands.command(name="pet")
     async def petpat(self, ctx, *, username: discord.Member = None):
         username = username or ctx.author
         avatar = username.avatar_url_as(format='png')
         url = f"https://some-random-api.ml/premium/petpet?avatar={avatar}&key=KdRrQKvpk35OfxNGIm997pEvC"
-        #print(url)
+        # print(url)
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as af:
                 if 300 > af.status >= 200:
                     fp = io.BytesIO(await af.read())
                     file = discord.File(fp, "petpat.gif")
                     await ctx.reply(file=file, mention_author=False)
-                    
+
                 else:
                     await ctx.reply('Oops! An error occured. Please Try Again Later.')
                     await session.close()
 
     @commands.command(name="fact")
     async def animal_facts(self, ctx):
-        embed=discord.Embed(title="<a:panda:871255104735567912> Animal Facts", timestamp=ctx.message.created_at, color=discord.Color.dark_orange(), description="Click on the drop down menu and select what fact do you want to know.")
+        embed = discord.Embed(title="<a:panda:871255104735567912> Animal Facts", timestamp=ctx.message.created_at,
+                              color=discord.Color.dark_orange(),
+                              description="Click on the drop down menu and select what fact do you want to know.")
         embed.set_thumbnail(url='https://media.wired.com/photos/593261cab8eb31692072f129/master/pass/85120553.jpg')
-        embed.set_author(name="Love animals", icon_url='https://cdn.discordapp.com/attachments/831369746855362590/831369994474094622/Logo.jpg')
+        embed.set_author(name="Love animals",
+                         icon_url='https://cdn.discordapp.com/attachments/831369746855362590/831369994474094622/Logo.jpg')
 
         urlPanda = "https://some-random-api.ml/animal/panda"
         responsePanda = requests.request("GET", url=urlPanda)
@@ -91,7 +96,8 @@ class Fun(commands.Cog):
         imagePanda = dataPanda['image']
         factPanda = dataPanda['fact']
 
-        embedPanda = discord.Embed(title="A Panda Fact", timestamp=ctx.message.created_at, color=discord.Color.dark_theme(), description=factPanda)
+        embedPanda = discord.Embed(title="A Panda Fact", timestamp=ctx.message.created_at,
+                                   color=discord.Color.dark_theme(), description=factPanda)
         embedPanda.set_footer(text='Delta Δ is the fourth letter of the Greek Alphabet', icon_url=ctx.author.avatar_url)
         embedPanda.set_image(url=imagePanda)
 
@@ -100,7 +106,8 @@ class Fun(commands.Cog):
         imageDog = responseDog.json()['image']
         factDog = responseDog.json()['fact']
 
-        embedDog = discord.Embed(title="A Dog Fact", timestamp=ctx.message.created_at, color=discord.Color.dark_grey(), description=factDog)
+        embedDog = discord.Embed(title="A Dog Fact", timestamp=ctx.message.created_at, color=discord.Color.dark_grey(),
+                                 description=factDog)
         embedDog.set_footer(text="Delta Δ is the fourth letter of the Greek Alphabet", icon_url=ctx.author.avatar_url)
         embedDog.set_image(url=imageDog)
 
@@ -109,7 +116,8 @@ class Fun(commands.Cog):
         imageFox = responseFox.json()['image']
         factFox = responseFox.json()['fact']
 
-        embedFox = discord.Embed(title="A Fox Fact", timestamp=ctx.message.created_at, color=discord.Color.dark_blue(), description=factFox)
+        embedFox = discord.Embed(title="A Fox Fact", timestamp=ctx.message.created_at, color=discord.Color.dark_blue(),
+                                 description=factFox)
         embedFox.set_footer(text="Delta Δ is the fourth letter of the Greek Alphabet", icon_url=ctx.author.avatar_url)
         embedFox.set_image(url=imageFox)
 
@@ -118,7 +126,8 @@ class Fun(commands.Cog):
         imageKoala = responseKoala.json()['image']
         factKoala = responseKoala.json()['fact']
 
-        embedKoala = discord.Embed(title="A Koala Fact", timestamp=ctx.message.created_at, color=discord.Color.dark_gold(), description=factKoala)
+        embedKoala = discord.Embed(title="A Koala Fact", timestamp=ctx.message.created_at,
+                                   color=discord.Color.dark_gold(), description=factKoala)
         embedKoala.set_footer(text="Delta Δ is the fourth letter of the Greek Alphabet", icon_url=ctx.author.avatar_url)
         embedKoala.set_image(url=imageKoala)
 
@@ -127,7 +136,8 @@ class Fun(commands.Cog):
         imageBird = responseBird.json()['image']
         factBird = responseBird.json()['fact']
 
-        embedBird = discord.Embed(title="A Bird Fact", timestamp=ctx.message.created_at, color=discord.Color.dark_purple(), description=factBird)
+        embedBird = discord.Embed(title="A Bird Fact", timestamp=ctx.message.created_at,
+                                  color=discord.Color.dark_purple(), description=factBird)
         embedBird.set_footer(text='Delta Δ is the fourth letter of the Greek Alphabet', icon_url=ctx.author.avatar_url)
         embedBird.set_image(url=imageBird)
 
@@ -138,11 +148,11 @@ class Fun(commands.Cog):
                 placeholder="Select a Fact Category",
                 min_values=1,
                 options=[
-                SelectOption(label="🐼 Panda", value="Select this to get a randomly generated Panda fact"),
-                SelectOption(label="🐶 Dog", value="Select this to get a randomly generated Dog fact"),
-                SelectOption(label="🦊 Fox", value="Select this to get a randomly generated Fox fact"),
-                SelectOption(label="🐨 Koala", value="Select this to get a randomly generated Koala fact"),
-                SelectOption(label="🐦 Bird", value="Select this to get a randomly generated Bird fact")
+                    SelectOption(label="🐼 Panda", value="Select this to get a randomly generated Panda fact"),
+                    SelectOption(label="🐶 Dog", value="Select this to get a randomly generated Dog fact"),
+                    SelectOption(label="🦊 Fox", value="Select this to get a randomly generated Fox fact"),
+                    SelectOption(label="🐨 Koala", value="Select this to get a randomly generated Koala fact"),
+                    SelectOption(label="🐦 Bird", value="Select this to get a randomly generated Bird fact")
                 ],
             ),
         ])
@@ -155,45 +165,59 @@ class Fun(commands.Cog):
             elif check_for.lower() == '🐶 dog':
                 await interaction.respond(embed=embedDog, ephemeral=False)
             elif check_for.lower() == '🦊 fox':
-                await interaction.respond(embed=embedFox, ephemeral=False) 
+                await interaction.respond(embed=embedFox, ephemeral=False)
             elif check_for.lower() == '🐨 koala':
                 await interaction.respond(embed=embedKoala, ephemeral=False)
             elif check_for.lower() == '🐦 bird':
                 await interaction.respond(embed=embedBird, ephemeral=False)
 
-
-
     # Error Handlers
-    @youtube_comment_fake.error 
+    @youtube_comment_fake.error
     async def yt_fake_error_handling(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            embed = discord.Embed(title="<:hellno:871582891585437759> Missing Arguments", description="```ini\nMake sure you have run the command providing the [username]* and the [comment]```", timestamp=ctx.message.created_at, color=discord.Color.dark_grey())
-            embed.set_footer(text="*Usernames mustn't contain spaces if they are typed instead of pinging or using id's", icon_url=ctx.author.avatar_url)
+            embed = discord.Embed(title="<:hellno:871582891585437759> Missing Arguments",
+                                  description="```ini\nMake sure you have run the command providing the [username]* and the [comment]```",
+                                  timestamp=ctx.message.created_at, color=discord.Color.dark_grey())
+            embed.set_footer(
+                text="*Usernames mustn't contain spaces if they are typed instead of pinging or using id's",
+                icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
 
         if isinstance(error, commands.MemberNotFound):
-            embed = discord.Embed(title="<:hellno:871582891585437759> Member Not Found", description="```ini\nMake sure you have run the command providing the [username]* and the [comment]```", timestamp=ctx.message.created_at, color=discord.Color.dark_grey())
-            embed.set_footer(text="*Usernames mustn't contain spaces if they are typed instead of pinging or using id's", icon_url=ctx.author.avatar_url)
+            embed = discord.Embed(title="<:hellno:871582891585437759> Member Not Found",
+                                  description="```ini\nMake sure you have run the command providing the [username]* and the [comment]```",
+                                  timestamp=ctx.message.created_at, color=discord.Color.dark_grey())
+            embed.set_footer(
+                text="*Usernames mustn't contain spaces if they are typed instead of pinging or using id's",
+                icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
 
-    @tweet_fake.error 
+    @tweet_fake.error
     async def tweet_fake_error_handling(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            embed = discord.Embed(title="<:hellno:871582891585437759> Missing Arguments", description="```ini\nMake sure you have run the command providing the [username]* and the [comment]```", timestamp=ctx.message.created_at, color=discord.Color.dark_grey())
-            embed.set_footer(text="*Usernames mustn't contain spaces if they are typed instead of pinging or using id's", icon_url=ctx.author.avatar_url)
+            embed = discord.Embed(title="<:hellno:871582891585437759> Missing Arguments",
+                                  description="```ini\nMake sure you have run the command providing the [username]* and the [comment]```",
+                                  timestamp=ctx.message.created_at, color=discord.Color.dark_grey())
+            embed.set_footer(
+                text="*Usernames mustn't contain spaces if they are typed instead of pinging or using id's",
+                icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
 
         if isinstance(error, commands.MemberNotFound):
-            embed = discord.Embed(title="<:hellno:871582891585437759> Member Not Found", description="```ini\nMake sure you have run the command providing the [username]* and the [comment]```", timestamp=ctx.message.created_at, color=discord.Color.dark_grey())
-            embed.set_footer(text="*Usernames mustn't contain spaces if they are typed instead of pinging or using id's", icon_url=ctx.author.avatar_url)
+            embed = discord.Embed(title="<:hellno:871582891585437759> Member Not Found",
+                                  description="```ini\nMake sure you have run the command providing the [username]* and the [comment]```",
+                                  timestamp=ctx.message.created_at, color=discord.Color.dark_grey())
+            embed.set_footer(
+                text="*Usernames mustn't contain spaces if they are typed instead of pinging or using id's",
+                icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
 
     @petpat.error
     async def petpat_error_handling(self, ctx, error):
         if isinstance(error, commands.MemberNotFound):
-            await ctx.send(embed=discord.Embed(title="<:hellno:871582891585437759> Member Not Found", description="```ini\nMake sure you have run the command providing the [username]```", timestamp=ctx.message.created_at, color=discord.Color.dark_teal()))
-
-
+            await ctx.send(embed=discord.Embed(title="<:hellno:871582891585437759> Member Not Found",
+                                               description="```ini\nMake sure you have run the command providing the [username]```",
+                                               timestamp=ctx.message.created_at, color=discord.Color.dark_teal()))
 
 
 def setup(client):
