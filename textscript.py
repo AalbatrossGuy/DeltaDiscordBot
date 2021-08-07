1dbe99a8f9ed9d98d0811c4000bf1f54948cb43c
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2021-08-07 08:04:49 +0530

    More test stuff

--- textscript.py
+++ textscript.py
@@ -1,10 +1,4 @@
-from PIL import Image, ImageDraw, ImageFilter, ImageFont
-import requests
-import datetime
-import qrcode
-from pyzbar.pyzbar import decode
-from matplotlib import pyplot as plt
-from currency_converter import CurrencyConverter
+import subprocess
 
 # print(image.size)
 # print(image.format)
@@ -260,6 +254,8 @@ from currency_converter import CurrencyConverter
 # stdin = re.findall(result, op)
 
 langtest = 'javascript'
+
+
 # for language, version in zip(language, versions):
 # print(f"{language}: v{version}")
 # regex(op)
@@ -325,3 +321,22 @@ langtest = 'javascript'
 # thumbnail = data['thumbnail']['genius']
 # print(lyrics)
 # # print(data)
+# def version_info():
+#     version = 'No Data'
+#     date = 'No Data'
+#     gitlog = subprocess.check_output(
+#         ['git', 'log', '-n', '1', '--date=iso']).decode()
+#     for line in gitlog.split('\n'):
+#         if line.startswith('commit'):
+#             version = line.split(' ')[1]
+#         elif line.startswith('Date'):
+#             date = line[5:].strip()
+#             date = date.replace(' +', '+').replace(' ', 'T')
+#         else:
+#             pass
+#     return version, date
+#
+#
+# version = version_info()[0][:]
+# print(version)
+# # 6bed3dd8
