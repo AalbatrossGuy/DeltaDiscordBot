83350da0c16e96638e7d8cd668f3a29b56add4db
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2021-10-29 19:25:43 +0530

    Refactored code

--- Cogs/utility.py
+++ Cogs/utility.py
@@ -14,68 +14,8 @@ from discord_components import DiscordComponents, Button, ButtonStyle
 from aiohttp import ClientSession
 from datetime import datetime, timezone, timedelta
 from currency_converter import CurrencyConverter
+from customs.buttons import Buttons, KillButtons
 
-Buttons = [
-    [
-        Button(style=ButtonStyle.grey, label='1'),
-        Button(style=ButtonStyle.grey, label='2'),
-        Button(style=ButtonStyle.grey, label='3'),
-        Button(style=ButtonStyle.blue, label='x'),
-        Button(style=ButtonStyle.red, label='Quit')
-    ],
-    [
-        Button(style=ButtonStyle.grey, label='4'),
-        Button(style=ButtonStyle.grey, label='5'),
-        Button(style=ButtonStyle.grey, label='6'),
-        Button(style=ButtonStyle.blue, label='÷'),
-        Button(style=ButtonStyle.red, label='⟵')
-    ],
-    [
-        Button(style=ButtonStyle.grey, label='7'),
-        Button(style=ButtonStyle.grey, label='8'),
-        Button(style=ButtonStyle.grey, label='9'),
-        Button(style=ButtonStyle.blue, label='+'),
-        Button(style=ButtonStyle.red, label='Clear')
-    ],
-    [
-        Button(style=ButtonStyle.grey, label='00'),
-        Button(style=ButtonStyle.grey, label='0'),
-        Button(style=ButtonStyle.blue, label='.'),
-        Button(style=ButtonStyle.blue, label='-'),
-        Button(style=ButtonStyle.red, label='Answ')
-    ],
-]
-
-KillButtons = [
-    [
-        Button(style=ButtonStyle.grey, label='1', disabled=True),
-        Button(style=ButtonStyle.grey, label='2', disabled=True),
-        Button(style=ButtonStyle.grey, label='3', disabled=True),
-        Button(style=ButtonStyle.blue, label='X', disabled=True),
-        Button(style=ButtonStyle.red, label='Quit', disabled=True)
-    ],
-    [
-        Button(style=ButtonStyle.grey, label='4', disabled=True),
-        Button(style=ButtonStyle.grey, label='5', disabled=True),
-        Button(style=ButtonStyle.grey, label='6', disabled=True),
-        Button(style=ButtonStyle.blue, label='÷', disabled=True),
-        Button(style=ButtonStyle.red, label='⟵', disabled=True)
-    ],
-    [
-        Button(style=ButtonStyle.grey, label='7', disabled=True),
-        Button(style=ButtonStyle.grey, label='8', disabled=True),
-        Button(style=ButtonStyle.grey, label='9', disabled=True),
-        Button(style=ButtonStyle.blue, label='+', disabled=True),
-        Button(style=ButtonStyle.red, label='Clear', disabled=True)
-    ],
-    [
-        Button(style=ButtonStyle.grey, label='00', disabled=True),
-        Button(style=ButtonStyle.grey, label='0', disabled=True),
-        Button(style=ButtonStyle.blue, label='.', disabled=True),
-        Button(style=ButtonStyle.blue, label='-', disabled=True),
-        Button(style=ButtonStyle.red, label='Answ', disabled=True)
-    ],
-]
 
 
 def calculate(expr):
