15a91b2e9fffde70c8b25d3c281fb5964f2cc792
AalbatrossGuy <68378354+AaalbatrossGuy@users.noreply.github.com>
2021-11-03 02:37:54 +0000

    Updated port number

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ def main():
   return "Bot up and running..."
 
 def run():
-  app.run(host="0.0.0.0", port=5000)
+  app.run(host="0.0.0.0", port=5001)
 
 def keep_alive():
   server = Thread(target=run)
