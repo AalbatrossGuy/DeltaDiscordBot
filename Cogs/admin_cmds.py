d35d5cf1467b7e36323b3e5945292abfcc729261
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-11-20 06:21:08 +0530

    Small Update

--- Cogs/admin_cmds.py
+++ Cogs/admin_cmds.py
@@ -14,15 +14,15 @@ class AdminCmds(commands.Cog):
     @commands.has_permissions(manage_messages=True)
     @commands.command(name="purge", aliases=['clear', 'delete'])
     async def delete_channel_messages(self, ctx, limit: int=10):
-        
+
         logch = db.field("SELECT LogChannelID FROM adminsettings WHERE GuildID = ?", ctx.guild.id)
-        await ctx.channel.purge(limit=limit + 1) 
+        await ctx.channel.purge(limit=limit + 1)
         await ctx.channel.send(f"<:salute:831807820118622258> Purged `{limit}` messages!")
         await asyncio.sleep(2)
         await ctx.channel.purge(limit=1)
 
         if logch != 0:
-            embed = discord.Embed(title="Log Message", timestamp=ctx.message.created_at, color=discord.Color.dark_orange(), 
+            embed = discord.Embed(title="Log Message", timestamp=ctx.message.created_at, color=discord.Color.dark_orange(),
                     description=f"{ctx.author.mention} used `purge` in <#{ctx.channel.id}> to delete {limit} messages.")
             embed.set_footer(text='Delta Δ is the fourth letter of the Greek Alphabet', icon_url=ctx.author.avatar_url)
             embed.set_thumbnail(url="https://pandorafms.com/blog/wp-content/uploads/2020/12/567-Logs_-qu%C3%A9-son-y-por-qu%C3%A9-monitorizarlos.jpg")
@@ -38,7 +38,7 @@ class AdminCmds(commands.Cog):
         await member.kick(reason=reason)
         await ctx.message.delete()
         await ctx.channel.send(f"<:pandacop:831800704372178944> Kicked {member.mention} successfully!")
-        
+
         if logch != 0:
             embed=discord.Embed(title="Log Message", timestamp=ctx.message.created_at, color=discord.Color.dark_orange(), description = f"{ctx.author.mention} used `kick` in <#{ctx.channel.id}> to **kick** {member.mention} because `{reason}`")
             embed.set_footer(text='Delta Δ is the fourth letter of the Greek Alphabet', icon_url=ctx.author.avatar_url)
@@ -82,7 +82,7 @@ class AdminCmds(commands.Cog):
     async def masskick(self, ctx, *users: discord.User):
         #print(type(users))
         logch = db.field("SELECT LogChannelID FROM adminsettings WHERE GuildID = ?", ctx.guild.id)
-        
+
         await asyncio.gather(*map(ctx.guild.kick, users))
         await ctx.channel.purge(limit=1)
         await ctx.channel.send(f"<:pandacop:831800704372178944> Kicked {len(users)} member/s successfully!")
@@ -99,7 +99,7 @@ class AdminCmds(commands.Cog):
     @commands.has_permissions(ban_members=True)
     async def massban(self, ctx, *users: discord.User):
         logch = db.field("SELECT LogChannelID FROM adminsettings WHERE GuildID = ?", ctx.guild.id)
-        
+
         await asyncio.gather(*map(ctx.guild.ban, users))
         await ctx.channel.purge(limit=1)
         await ctx.channel.send(f"<:pandacop:831800704372178944> Banned {len(users)} member/s successfully!")
@@ -151,7 +151,7 @@ class AdminCmds(commands.Cog):
         except Exception as e:
             print(e)
             await ctx.channel.send("Make sure the bot has permission to read/send messages in the log channel and also provide the ChannelID correctly. ID must be an int.")
-    
+
 
     @commands.has_permissions(manage_channels=True)
     @commands.command(name='deletelogch')
@@ -160,9 +160,9 @@ class AdminCmds(commands.Cog):
         check_guild_exists = db.cursor.execute("SELECT 1 FROM adminsettings WHERE GuildID = ?", (ctx.guild.id,))
         #print('got check_guild_exists')
         before_delete = db.field("SELECT LogChannelID FROM adminsettings WHERE GuildID = ?", ctx.guild.id)
-        #print('got before_delete') 
+        #print('got before_delete')
         check_guild_exists = check_guild_exists.fetchone() is None
-        #print('got check_guild_exists') 
+        #print('got check_guild_exists')
         if check_guild_exists == True and before_delete !=0:
             #print('check done')
             db.execute("UPDATE adminsettings SET LogChannelID = ? WHERE GuildID = ?", 0, ctx.guild.id)
@@ -170,7 +170,7 @@ class AdminCmds(commands.Cog):
             db.commit()
             #print('saved')
             await ctx.channel.send(f"Won't log messages in <#{before_delete}> anymore!")
-    
+
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         db.execute("DELETE FROM adminsettings WHERE GuildID = ?", guild.id)
@@ -178,12 +178,20 @@ class AdminCmds(commands.Cog):
         print('removed guild from adminsettings table')
 
 
-    @set_log_channel.error 
+    @commands.is_owner()
+    @commands.command()
+    async def servers(self, ctx):
+        activeservers = self.client.guilds
+        for guild in activeservers:
+            await ctx.send(guild.name)
+            await ctx.channel.send(f"{guild.name} - {guild.owner}")
+
+    @set_log_channel.error
     async def setlogch_error_handling(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send(embed=discord.Embed(title="<:hellno:871582891585437759> Missing Permissions", description="```prolog\nYou must have the Manage Channels permission to use that command!```", timestamp=ctx.message.created_at, color=discord.Color.magenta()))
 
-    @delete_log_channel.error 
+    @delete_log_channel.error
     async def deletelogch_error_handling(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send(embed=discord.Embed(title="<:hellno:871582891585437759> Missing Permissions", description="```prolog\nYou must have the Manage Channels permission to use that command!```", timestamp=ctx.message.created_at, color=discord.Color.magenta()))
@@ -235,8 +243,8 @@ class AdminCmds(commands.Cog):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send(embed=discord.Embed(title="<:hellno:871582891585437759>  Missing Permissions", description='```prolog\nYou must have the Ban Members permission to use that command!```', timestamp=ctx.message.created_at, color=discord.Color.gold()))
 
-    
-#     @commands.command(name="lockdown") 
+
+#     @commands.command(name="lockdown")
 #     @commands.has_guild_permissions(manage_channels=True)
 #     @commands.bot_has_guild_permissions(manage_channels=True)
 #     async def lockdown(self, ctx, channel:discord.TextChannel=None):
@@ -246,29 +254,29 @@ class AdminCmds(commands.Cog):
 #             await channel.edit(overwrites=overwrites)
 #             print('done #1')
 #             await ctx.send(f"`{channel.name}` is on Lockdown!!!")
-# 
+#
 #         elif channel.overwrites[ctx.guild.default_role].send_messages == True or channel.overwrites[ctx.guild.default_role].send_messages == None:
 #             overwrites = channel.overwrites[ctx.guild.default_role]
 #             overwrites.send_messages = False
 #             await channel.set_permissions(ctx.guild.default_role, overwrite=overwrites)
 #             print('done #2')
 #             await ctx.send(f"`{channel.name}` is on Lockdown!!!")
-#             
-# 
-# 
-# 
+#
+#
+#
+#
 #     @commands.has_guild_permissions(manage_channels=True)
 #     @commands.bot_has_guild_permissions(manage_channels=True)
 #     @commands.command(name="unlock")
 #     async def unlock(self, ctx):
 #         await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
 #         await ctx.send(ctx.channel.name + " has been unlocked.")
-        
+
 #     @lockdown.error
 #     async def lockdown_error_handling(self, ctx, error):
 #         if isinstance(error, commands.MissingPermissions):
 #             await ctx.send(embed=discord.Embed(title="<:hellno:871582891585437759> Missing Permissions", description="```prolog\nYou must have the Manage Channels permission to use that command!```", timestamp=ctx.message.created_at, color=discord.Color.dark_grey()))
-# 
+#
 #     @unlock.error
 #     async def unlock_error_handling(self, error, ctx):
 #         if isinstance(error, commands.MissingPermissions):
